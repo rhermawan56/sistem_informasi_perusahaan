3304dd41e756915050e77f1dd5630d4602f3bbd6
inalzainal <inal.zainalmuttaqien145@gmail.com>
2019-12-08 03:28:40 +0700

    add locale id

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-baris bahasa untuk autentifikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentifikasi untuk beberapa
+    | pesan yang perlu kita tampilkan ke pengguna. Anda bebas untuk memodifikasi
+    | baris bahasa sesuai dengan keperluan aplikasi anda.
+    |
+    */
+
+    'failed' => 'Kredensial ini tidak cocok dengan catatan kami.',
+    'throttle' => 'Terlalu banyak upaya masuk. Silakan coba lagi dalam :seconds detik.',
+
+];
